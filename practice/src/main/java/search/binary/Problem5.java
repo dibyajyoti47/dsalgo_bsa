44094218e146855a648ce7e1f855219262c8a0fa
dj <dj@Ubuntu-22.myguest.virtualbox.org>
2025-01-29 13:59:38 +0530

    ipdated

--- practice/src/main/java/search/binary/Problem5.java
+++ practice/src/main/java/search/binary/Problem5.java
@@ -9,6 +9,37 @@ package search.binary;
 
 public class Problem5 {
     public static void run() {
-
+        int arr[] = {3, 3, 1, 1, 8, 8, 6, 10, 10, 2, 2, 4, 4};
+        System.out.println("Found odd element at index : "+ doBinarySearch(arr));
     }
+    private static int doBinarySearch(int[] arr) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (mid == arr.length -1 || mid == 0) {
+                // this means the odd element is either
+                // on the first index or last index
+                return mid;
+            }
+            if (arr[mid] != arr[mid+1] && arr[mid] != arr[mid-1]) {
+                return mid;
+            }
+            if (mid%2 == 0) {
+                if (arr[mid] == arr[mid+1]) {
+                    low = mid+1;
+                } else {
+                    high = mid-1;
+                }
+            } else {
+                if (arr[mid] == arr[mid+1]) {
+                    high = mid-1;
+                } else {
+                    low = mid+1;
+                }
+            }
+        }
+        return -1;
+    }
+
 }
