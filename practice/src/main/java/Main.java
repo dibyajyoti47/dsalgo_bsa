f326054d302eb6ba120babf26f44d3e90dcbb0ff
dj <dj@Ubuntu-22.myguest.virtualbox.org>
2025-01-29 15:19:34 +0530

    committing

--- practice/src/main/java/Main.java
+++ practice/src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    search.binary.Problem5.run();
+    search.binary.Problem6.run();
   }
 
 }
--- practice/src/main/java/search/binary/Problem6.java
+++ practice/src/main/java/search/binary/Problem6.java
@@ -1,8 +1,29 @@
 package search.binary;
 
 /**
- *
+ * Given a number N, find the floor that is (square root of N)
+ *  {2,3,5, 6, 7,8,9,10,13,14,16,17}  target: 101
  */
 public class Problem6 {
-
+    public static void run() {
+        int[] arr = {2,3,5,6,7,9,10,11,13,14};
+        System.out.println(" Found at : "+ doBinarySearch(arr, 196));
+    }
+    public static int doBinarySearch(int[] arr, int target) {
+        int n = arr.length;
+        int ans = -1;
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid]*arr[mid] == target) return mid;
+            if (arr[mid] * arr[mid] < target) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
 }
